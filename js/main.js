d9d30b18eac14f0b134aa71e930af700116b56ea
Christophe Coenraets <ccoenraets@gmail.com>
2013-04-23 12:52:01 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var app = {
+
+    findByName: function() {
+        this.store.findByName($('.search-key').val()).done(function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
+
+    initialize: function() {
+        this.store = new MemoryAdapter();
+        this.store.initialize().done(function() {
+            console.log("Data store initialized");
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
+
+};
+
+app.initialize();
